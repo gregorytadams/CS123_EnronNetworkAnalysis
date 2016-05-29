3d965c664513bd9e61c4e392e44fead69849d3cb
Jonathan Kyl <jkyl@cs.uchicago.edu>
2016-05-28 20:24:56 -0500

    yep

--- text_sim.py
+++ text_sim.py
@@ -2,29 +2,34 @@ from gensim import corpora, similarities, models
 from collections import defaultdict
 import os
 import logging
+import numpy as np
+import random
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 stoplist = set('for a of the and or to in and as at from'.split())
 
-class Dict(object):
-    def __init__(self, fnames):
-        '''
-        '''
-        self.fnames = fnames
-        self.d = corpora.Dictionary(open(f).read().lower().split() for f in fnames)
-        stop_ids = [self.d.token2id[stopword] for stopword in stoplist
-                    if stopword in self.d.token2id]
-        once_ids = [tokenid for tokenid, docfreq in self.d.dfs.items()
-                    if docfreq == 1]
-        self.d.filter_tokens(stop_ids + once_ids)
-        self.d.compactify()
-     
+def tokenize(fname):
+    return [word for word in open(fname).read().lower().split() if (not word in stoplist)]
+
+def build_dict(fnames):
+    files_gen = (tokenize(f) for f in fnames)
+    d = corpora.Dictionary(i for i in files_gen) 
+    once_ids = [tokenid for tokenid, docfreq in d.dfs.items() if docfreq == 1]
+    d.filter_tokens(once_ids)
+    d.compactify()
+    return d
+
+def gen_paths(dir):
+    for roots, dirs, files in os.walk(dir):
+        yield (os.path.join(root, name) for name in files)
+
 class Comparitor():
-    def __init__(self, fnames):
+    def __init__(self, train_fnames, test_fnames):
         '''
         '''
-        self.fnames = fnames
-        self.D = Dict(fnames)
+        self.train_fnames = train_fnames
+        self.test_fnames = test_fnames
+        self.d = build_dict(train_fnames)       
         
         corpora.MmCorpus.serialize('/tmp/corpus.mm', (v for v in self))
         self.corpus = corpora.MmCorpus('/tmp/corpus.mm')
@@ -32,18 +37,14 @@ class Comparitor():
         models.TfidfModel(self.corpus).save('/tmp/model.tfidf')
         self.corpus_tfidf = models.TfidfModel.load('/tmp/model.tfidf')[self.corpus]
 
-        models.RpModel(self.tfidf[self.corpus], num_topics=500).save('/tmp/model.rp')
-        self.corpus_rp = models.RpModel.load('/tmp/model.rp')[self.corpus_tfidf]
-        
-        
+        #models.RpModel(self.tfidf[self.corpus], num_topics=500).save('/tmp/model.rp')
+        #self.corpus_rp = models.RpModel.load('/tmp/model.rp')[self.corpus_tfidf]
+       
     def __iter__(self):
         '''
         '''
-        for f in self.fnames:
-            yield self.D.d.doc2bow(open(f).read().lower().split())
-
-    
-        
+        for f in self.test_fnames:
+            yield self.d.doc2bow(tokenize(f))
 
         
     
