b8d876b1563263a0be27d6b02034df93b42d6dab
gnorthrup <gnorthrup@uchicago.edu>
2016-05-30 17:07:13 -0500

    Commented bash script

--- setup_instance.sh
+++ setup_instance.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+# Once the volume is mounted, gets pip and 
+# installs all of packages needed for text_sim.py
+# Also installs sqlite3 and unzip for old project
+# EC2 setup
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
 sudo python3 -m pip install --upgrade numpy
