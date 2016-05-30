3d01f3ea7e293489fb08181c41c50dd5111167a4
gnorthrup <gnorthrup@uchicago.edu>
2016-05-30 16:28:18 -0500

    Added docstring

--- consolidate.py
+++ consolidate.py
@@ -2,6 +2,11 @@ import os, sys, csv
 from shutil import copyfile
 
 for f in os.listdir('output/'):
+    '''
+    takes the files listed in a csv
+    and moves them into a new directory
+    called consolidated_output
+    '''
     print(f)
     with open('output/' + f) as csvfile:
         reader = csv.reader(csvfile)
