bdaf9becb359475c02c700b3db2d54393f826ac0
Jonathan Kyl <jkyl@Jonathans-MacBook-Air.local>
2016-05-30 15:49:35 -0500

    added saving as comparitor method

--- text_sim.py
+++ text_sim.py
@@ -132,7 +132,7 @@ if __name__ == '__main__':
     parser.add_option('-k', '--topk', dest='topk', help='Size of top-k heap', default=100)
     parser.add_option('-n', '--ndims', dest='ndim', help='Dimensionality of LSI model', default=200)
     parser.add_option('-t', '--train', dest='ndim', help='Number of training sample files', default=1000)
-    parser.add_option('-s', '--seed', dest='seed', help='Random seed for sampling', default=666)
+    parser.add_option('-s', '--seed', dest='seed', help='Seed for training data sample', default=666)
     opts, args = parser.parse_args()
     c = Comparitor(args['path'], args['outf'])
     c.top_k(args['topk'], save=True)
