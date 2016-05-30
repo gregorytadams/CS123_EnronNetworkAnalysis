1fb8a260be0d16b0e65534cc0fcb4bf2f0d994c9
gnorthrup <gnorthrup@uchicago.edu>
2016-05-30 16:26:41 -0500

    Docstring added

--- agg_best.py
+++ agg_best.py
@@ -2,6 +2,11 @@ import os
 import csv
 
 def combine_csvs():
+	'''
+	when run in a directory of csvs
+	takes all of the lines and writes them to one csv file
+	called masterfile.csv
+	'''
 	with open("masterfile.csv", 'w') as f:
 		for subfile in next(os.walk('.'))[2]:
 			with open(subfile, 'r') as f2:
